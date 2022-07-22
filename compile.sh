f5db9880f0ddb7e0dadd7f5bd1e2826d90d53e6d
Seren541 <gholmann16@gmail.com>
2022-07-22 16:13:24 -0400

    Licences

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp /usr/local/lib/libneptune.so src/Neptune.AppDir/usr/lib/libneptune.so
 gcc src/AppRun.c -o src/Neptune.AppDir/AppRun -Wno-format-truncation
-gcc src/main.c -lappimage -liup -lneptune -o src/Neptune.AppDir/usr/bin/Neptune
+gcc src/main.c -lappimage -lezxml -liup -lneptune -o src/Neptune.AppDir/usr/bin/Neptune
 appimagetool src/Neptune.AppDir/ > build.output
